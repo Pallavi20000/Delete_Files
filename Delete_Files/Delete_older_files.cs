fa5dea7404e58c438b9db3c8be8457abf2b70776
PallaviS <PallaviS@LPTIN-5LS1063>
2023-02-16 22:27:56 +0530

    Add project files.

---
+++ Delete_Files/Delete_older_files.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+
+namespace Delete_Files
+{
+    internal class Delete_older_files
+    {
+        static void Main(string[] args)
+        {
+            string PathValue = ConfigurationManager.AppSettings["Path"];
+            string DaysValue = ConfigurationManager.AppSettings["Days"];
+            DirectoryInfo yourRootDir = new DirectoryInfo(PathValue);
+            foreach (FileInfo file in yourRootDir.GetFiles())
+            {
+                if (file.LastWriteTime < DateTime.Now.AddDays(-Convert.ToDouble(DaysValue)))
+                    file.Delete();
+                Console.WriteLine("Deleted the files from path { "+PathValue+" } which are older then "+DaysValue+ " days");
+            }
+            Console.ReadKey();
+        }
+    }
+}
